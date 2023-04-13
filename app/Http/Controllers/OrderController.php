d6d7315ceed8cb0904780eaf7e43dd353c98ab63
Juan <juanmarten99@gmail.com>
2023-04-13 20:58:36 -0300

    create controllers and test routes

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function allOrders(){
+        $orders = Order::all();
+        foreach($orders as $order){
+            echo $order->client()->get() . "<br>";
+            echo $order . "<br>";
+            echo $order->orderDetails()->get() . "<br><br>";
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/allcategories', [CategoryController::class, 'allCategories']);
+Route::get('/allclients', [ClientController::class, 'allClients']);
+Route::get('/allorders', [OrderController::class, 'allOrders']);
+Route::get('/allorderdetails', [OrderDetailController::class, 'allOrderDetails']);
+Route::get('/allproducts', [ProductController::class, 'allProducts']);
+Route::get('/allusers', [UserController::class, 'allUsers']);
