c2361e3748e418c2dd88d25c6624816503e56b49
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-04-19 11:15:20 -0300

    Breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ClientController;
@@ -23,6 +24,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::resource('products', ProductController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('orders', OrderController::class);
@@ -36,3 +47,5 @@ Route::get('/allproducts', [ProductController::class, 'allProducts']);
 Route::get('/allusers', [UserController::class, 'allUsers']);
 
 Route::get('/testdestroy', [CategoryController::class, 'testdestroy']);
+
+require __DIR__.'/auth.php';
