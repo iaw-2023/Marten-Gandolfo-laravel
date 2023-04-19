e81cf6d293a79b920916a01ecd6bad51fc662ee8
Juan <juanmarten99@gmail.com>
2023-04-18 22:23:57 -0300

    custom error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.baseTemplate')
+@include('components.navbar', ['activeLink' => ''])
+
+@section('css')
+<link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+
+
+@section('contenido')
+<h1>Error, página no encontrada</h1>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.baseTemplate')
+@include('components.navbar', ['activeLink' => ''])
+
+@section('css')
+<link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+
+
+@section('contenido')
+<h1>Error del servidor</h1>
+@endsection
