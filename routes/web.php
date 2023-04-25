e0f7c5ced3b9cc64f9e8d0006603abb95da7204c
Juan <juanmarten99@gmail.com>
2023-04-25 17:23:32 -0300

    logo on vercel

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::resource('orders', OrderController::class)->middleware(['auth']);
 Route::resource('clients', ClientController::class)->middleware(['auth']);
 Route::get('/orders/{id}/details', [OrderController::class, 'details'])->middleware(['auth']);
 
-Route::get('/api/logo', function () {
+Route::get('/logo', function () {
     return response()->file('logo.png');
 });
 
