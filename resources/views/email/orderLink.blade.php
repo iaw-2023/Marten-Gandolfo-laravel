a1c2d411982ba0358e8486b588e3b428afc5f8ac
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-05-22 14:52:37 -0300

    email style update

--- resources/views/email/orderLink.blade.php
+++ resources/views/email/orderLink.blade.php
@@ -1,13 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-    <title>Token de compra Master Gaming</title>
-</head>
-<body>
-    <h1>Hola {{ $order->client->email }},</h1>
-    <p>Este correo electronico contiene el token para acceder a la informacion de su pedido.</p>
-    <p>Token: {{ $order->token }}</p>
-    <p>Puede consultar con atencion al cliente con el token anterior o bien verlo usted mismo desde el siguiente enlace.</p>
-    <a href="http://localhost:3000/orders/{{ $order->token }}/details" class="btn btn-primary" role="button">Orden</a>
-</body>
+
+    <body>
+        <div>
+            <div>
+                <p>Muchas gracias por su compra en Master Gaming!</p>
+                <p>A continuacion se le brinda un <b>TOKEN</b> relacionado a su pedido.</p>
+                <h2>{{ $order->token }}</h2>
+                <p>Puede utilizarlo en nuestra pagina web, o bien ir directamente visualizar su compra en el siguiente enlace.</p>
+                <a href="http://localhost:3000/orders/{{ $order->token }}/details">Presione aqui para ver su compra</a>
+                <p></p>
+            </div>
+
+            <div>
+                Equipo de Master Gaming
+            </div>
+        </div>
+    </body>
+
 </html>
