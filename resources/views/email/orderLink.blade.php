fcd04977c2fefa4a268e3b302b8db44d54bdcd8e
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-05-29 21:49:16 -0300

    updated url of email order

--- resources/views/email/orderLink.blade.php
+++ resources/views/email/orderLink.blade.php
@@ -8,7 +8,7 @@
                 <p>A continuacion se le brinda un <b>TOKEN</b> relacionado a su pedido.</p>
                 <h2>{{ $order->token }}</h2>
                 <p>Puede utilizarlo en nuestra pagina web, o bien ir directamente visualizar su compra en el siguiente enlace.</p>
-                <a href="http://localhost:3000/orders/{{ $order->token }}/details">Presione aqui para ver su compra</a>
+                <a href="https://mastergaming-iaw2023.vercel.app/orders/{{ $order->token }}/details">Presione aqui para ver su compra</a>
                 <p></p>
             </div>
 
