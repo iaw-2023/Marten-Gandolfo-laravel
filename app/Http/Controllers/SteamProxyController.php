01a12963f0065772cd9aa5aba4e1c650fa05d1c0
Juan <juanmarten99@gmail.com>
2023-06-09 20:54:01 -0300

    steam api test

--- app/Http/Controllers/SteamProxyController.php
+++ app/Http/Controllers/SteamProxyController.php
@@ -9,7 +9,7 @@ class SteamProxyController extends Controller
 {
     public function indexApi(){
         $response = Http::withoutVerifying()->get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')->json()['applist']['apps'];
-        return response()->json(array_slice($response, 0, 90000));
+        return response()->json(array_slice($response, 0, 85000));
     }
 
     public function featuredApi(){
