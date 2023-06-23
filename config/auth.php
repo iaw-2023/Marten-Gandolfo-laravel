ea8fbdec4c4a70071c77c206732989c48a550c75
Juan <juanmarten99@gmail.com>
2023-06-23 12:37:22 -0300

    fix login

--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'clients',
+            'provider' => 'users',
         ],
 
         'clients' => [
